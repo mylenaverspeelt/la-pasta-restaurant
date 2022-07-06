c75a191850b5fda39ca07f8ad76e05f5cd4b88b4
mylenaverspeelt <mylena49@hotmail.com>
2022-07-06 20:54:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Menu from './pages/Menu/Menu';
+import './App.css';
+
+function App() {
+  return (
+<Menu />
+  );
+}
+
+export default App;
---
+++ src/pages/Menu/Menu.tsx
@@ -0,0 +1,9 @@
+import style from "./Menu.module.scss";
+
+export default function Menu() {
+  return (
+    <main>
+      <nav className={style.menu}>vamove</nav>
+    </main>
+  );
+}
