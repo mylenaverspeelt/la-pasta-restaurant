2887ba77a420c7f9d4078d177a4c29883475243d
mylenaverspeelt <mylena49@hotmail.com>
2022-07-07 09:41:06 -0300

    create header and css variables

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -6,8 +6,12 @@ export default function Menu() {
     <main>
       <nav className={style.menu}>
         <Logo />
-
       </nav>
+      <header className={style.header}>
+        <div className={style.header__text}>
+A melhor massa da região!
+        </div>
+      </header>
     </main>
   );
 }
