b5a50c644cad47fd91ea197b0b3452325030d529
mylenaverspeelt <mylena49@hotmail.com>
2022-07-08 11:10:56 -0300

    create searcher, set search state, css variables and styles

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,17 +1,25 @@
 import style from "./Menu.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Searcher from "./Searcher/Searcher";
+import { useState } from "react";
 
 export default function Menu() {
+
+    const [search, setSearch] = useState('')
+  
   return (
     <main>
       <nav className={style.menu}>
         <Logo />
       </nav>
       <header className={style.header}>
-        <div className={style.header__text}>
-A melhor massa da região!
-        </div>
+        <div className={style.header__text}>Authentic Italian Cuisine</div>
       </header>
+      <section className={style.menu}>
+        <h3 className={style.menu__title}>Menu</h3>
+        <Searcher search={search} setSearch={setSearch}/>
+  
+      </section>
     </main>
   );
 }
---
+++ src/pages/Menu/Searcher/Searcher.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import style from './Searcher.module.scss'
+import { CgSearch } from 'react-icons/cg'
+
+interface Props {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Searcher({search, setSearch}: Props) {
+  return <div className={style.buscador}>
+    <input onChange={(e) =>{
+        setSearch(e.target.value)
+    }} value={search} />
+    <CgSearch size={20} color='#4C4D5E' />
+  </div>;
+}
