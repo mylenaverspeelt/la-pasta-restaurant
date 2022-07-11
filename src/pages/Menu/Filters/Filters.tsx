ee026a39abfaf46a6a3c9a3cb0d441b5c93159b2
mylenaverspeelt <mylena49@hotmail.com>
2022-07-11 14:17:30 -0300

    create filters (component,  css module and json)

--- src/pages/Menu/Filters/Filters.tsx
+++ src/pages/Menu/Filters/Filters.tsx
@@ -1,5 +1,20 @@
+import filters from './filters.json'
+import style from './Filters.module.scss'
+type IOption = typeof filters[0]
+
 export default function Filters(){
+
+function selectFilters(option: IOption){
+
+}
+
     return(
-        <></>
+        <div className={style.filtros}>
+        {filters.map((option) => (
+            <button  className= {style.filtros__filtro}key={option.id} onClick={() => selectFilters(option)}>
+                {option.label}
+            </button>
+        ))} 
+        </div>
     )
 }
