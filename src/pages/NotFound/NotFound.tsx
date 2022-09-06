ea2c189499891d5da5da813f25be5eb5bfcdab2e
mylenaverspeelt <mylena49@hotmail.com>
2022-09-06 11:31:03 -0300

    feat: goBack button/useNavigate

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,13 +1,24 @@
 import style from "./NotFound.module.scss";
-import {ReactComponent as NotFoundImage} from "assets/not_found.svg";
+import { ReactComponent as NotFoundImage } from "assets/not_found.svg";
 import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
-  return(
-    <div className={classNames({
-      [style.container]: true
-    })}>
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className={classNames({
+        [style.container]: true,
+      })}
+    >
+      <button className={style.voltar} onClick={() => navigate(-1)}>
+        {" < Go back"}
+      </button>
+
       <NotFoundImage />
     </div>
   );
 }
+
+//useNavigate é um hook do react-router-dom pra conseguir acessar as camadas das rotas que o usuário faz. nesse caso o -1 é uma camada a menos, literalmente voltar à pagina anterior que o usuario tava.
